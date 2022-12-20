059fd51ebb8185694d85c2926adb776cf9bf955b
Michael Michel <michael.michel.hh@gmail.com>
2022-12-20 18:39:56 +0100

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,48 @@
-var gulp     = require('gulp'),
-    header   = require('gulp-header'),
-    uglify   = require('gulp-uglify'),
-    cleanCSS = require('gulp-clean-css'),
-    rename   = require('gulp-rename'),
-    package  = require('./package.json'),
-    banner;
+const gulp = require('gulp');
+const header = require('gulp-header');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const lebab = require('gulp-lebab');
+const package = require('./package.json');
+let banner;
 
-banner = [
-  '/*! ',
-    '<%= package.name %> ',
-    'v<%= package.version %> | ',
-    '(c) ' + new Date().getFullYear() + ' <%= package.author %> |',
-    ' <%= package.homepage %>',
-  ' */',
-  '\n'
-].join('');
+banner = `/*!
+ * <%= package.name %>
+ * v<%= package.version %> |
+ * (c) ${new Date().getFullYear()} <%= package.author %> |
+ * <%= package.homepage %>
+ */
+`;
 
-gulp.task('compress:js', function() {
+gulp.task('modernize:js', function () {
   return gulp.src('src/mediabox.js')
-    .pipe(header(banner, { package : package }))
+    .pipe(lebab())
+    .pipe(gulp.dest('src/'));
+});
+
+gulp.task('compress:js', function () {
+  return gulp.src('src/mediabox.js')
+    .pipe(header(banner, {package: package}))
     .pipe(gulp.dest('dist/'))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(header(banner, { package : package }))
+    .pipe(header(banner, {package: package}))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('compress:css', function() {
+gulp.task('compress:css', function () {
   return gulp.src('src/mediabox.css')
-    .pipe(header(banner, { package : package }))
+    .pipe(header(banner, {package: package}))
     .pipe(gulp.dest('dist/'))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({suffix: '.min'}))
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/mediabox.js', ['compress:js']);
-    gulp.watch('src/mediabox.css', ['compress:css']);
+  gulp.watch('src/mediabox.js', ['compress:js']);
+  gulp.watch('src/mediabox.css', ['compress:css']);
 });
 
 gulp.task('default', gulp.parallel('compress:js', 'compress:css'));
